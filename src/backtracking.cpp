c22f0e5e8f1ea14183189e818f1c38590a3f0519
Seth Pefley <pefleys@eos04.cis.gvsu.edu>
2018-04-22 20:58:57 -0400

    .

--- src/backtracking.cpp
+++ src/backtracking.cpp
@@ -104,7 +104,8 @@ bool solve(int board[9][9], int row, int col) {
 }
 
 int main(int argc, char** argv){
-	int board[9][9] = {{ 0, 3, 0, 0, 0, 0, 0, 2, 0 },
+	int board[9][9] = {
+		{ 0, 3, 0, 0, 0, 0, 0, 2, 0 },
 		{ 0, 9, 0, 0, 0, 0, 0, 8, 5 },
 		{ 5, 0, 0, 0, 8, 0, 4, 0, 0 },
 		{ 4, 0, 7, 2, 0, 6, 8, 9, 0 },
